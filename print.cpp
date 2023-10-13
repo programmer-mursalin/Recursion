ac0163e6513fac82fabae0ba59b33685adb62bc7
Mursalin Hawlader <programmer.mursalin016@gmail.com>
2023-10-13 09:41:49 +0600

    Create print.cpp

---
+++ print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void print(int n) {
+  if(n==1){
+    cout<<1<<endl;
+    return ;
+  }
+  cout<<n<<endl;
+  print(n-1);
+}
+
+int main() {
+  int n;
+  cin>>n;
+ print(n);
+}
